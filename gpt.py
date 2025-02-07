e335d87c783d26a77b061b42595925d0c9e63c40
Pratyush Jha <107312726+pratyush981@users.noreply.github.com>
2025-02-08 00:15:37 +0530

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -26,7 +26,6 @@ root.geometry('1000x1000')
 frame_1 = customtkinter.CTkFrame(master=root)
 frame_1.pack(pady=20, padx=60, fill="both", expand=True)
 
-# Create a label for displaying the video
 video_label = customtkinter.CTkLabel(master=frame_1)
 video_label.pack(anchor='nw')  # Use 'nw' anchor to place the video frame in the top left corner
 
